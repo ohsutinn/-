67f48e21d752177e248fd8f53e9697f1a7beb850
오세창 <114675677+sepang-pang@users.noreply.github.com>
2025-05-31 20:41:36 +0900

    init commit

---
+++ src/main/java/com/emoself/EmoSelfApplication.java
@@ -0,0 +1,13 @@
+package com.emoself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmoSelfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmoSelfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emoself/EmoSelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emoself;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmoSelfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
